f4d4802bce3ff77e7e893b015eaf3f4535bbc7ba
berka <ayowaberka@gmail.com>
2020-02-19 18:34:11 +0200

    added

---
+++ CustomElement/Notify.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CustomElement
+{
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:Notify runat=server></{0}:Notify>")]
+    public class Notify : WebControl
+    {
+
+        protected override void OnInit(EventArgs e)
+        {
+
+            if (!Page.IsClientScriptBlockRegistered("MyNotifyScript"))
+            {
+
+                string JavaScriptCode = @"<script>
+                function SomeJavaScriptFunction()
+                {
+                  // JavaScript code goes here
+                }
+                </script>";
+                Page.RegisterClientScriptBlock("MyNotifyScript", JavaScriptCode);
+            }
+  
+            base.OnInit(e);
+        }
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public string Text
+        {
+            get
+            {
+                String s = (String)ViewState["Text"];
+                return ((s == null) ? String.Empty : s);
+            }
+
+            set
+            {
+                ViewState["Text"] = value;
+            }
+        }
+
+
+        [Bindable(true)]
+        [DefaultValue("")]
+        public string Title
+        {
+            get
+            {
+                String s = (String)ViewState["Title"];
+                return ((s == null) ? String.Empty : s);
+            }
+
+            set
+            {
+                ViewState["Title"] = value;
+            }
+        }
+
+        protected override void RenderContents(HtmlTextWriter output)
+        {
+            output.Write(Title);
+        }
+
+        public void Show()
+        {
+
+        }
+    }
+}
---
+++ WebFormTest/WebFormTest/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebFormTest._Default" %>
+<%@ Register Namespace="CustomElement" Assembly="CustomElement" TagPrefix="axz" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <axz:Notify ID="notification" Title="Notification" />
+</asp:Content>
